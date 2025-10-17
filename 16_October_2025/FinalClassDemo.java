bba3d340811d3c47b8db93c44e70d0c3c125e360
Darshan Suresh <darshansuresh1804@gmail.com>
2025-10-17 13:21:40 +0530

    Add files via upload

---
+++ 16_October_2025/FinalClassDemo.java
@@ -0,0 +1,21 @@
+/*
+Exercise 13 - protected member variables
+03) Demonstrate a final method inside a final class - try to inherit the class - record the type of error, if the program projects one?
+ */
+package On1610;
+
+final class FinalClass {
+    final void display() {
+        System.out.println("Inside final class and final method.");
+    }
+}
+
+// Uncommenting this will cause a compilation error
+// class AttemptedChild extends FinalClass { }
+
+public class FinalClassDemo {
+    public static void main(String[] args) {
+        FinalClass obj = new FinalClass();
+        obj.display();
+    }
+}
