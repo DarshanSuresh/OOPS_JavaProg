5125d05389ec5f5294e5ef8da486dcaa0cca335e
Darshan Suresh <darshansuresh1804@gmail.com>
2025-11-13 13:50:51 +0530

    Add files via upload

---
+++ 25_September_2025/trickyops.java
@@ -0,0 +1,12 @@
+/*
+Tricky Operations
+ */
+package On2509;
+
+public class trickyops { 
+    public static void main(String[] args) { 
+       int a =1; 
+       a=a++ + ++a *a--; 
+       System.out.println(a); 
+    }
+}
