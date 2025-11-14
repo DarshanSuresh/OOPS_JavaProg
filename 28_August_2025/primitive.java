6386077080941c8ec62249f3193c7a15d152221f
Darshan Suresh <darshansuresh1804@gmail.com>
2025-11-14 15:06:22 +0530

    Add files via upload

---
+++ 28_August_2025/primitive.java
@@ -0,0 +1,24 @@
+
+package On2808;
+
+public class primitive { 
+    public static void main(String[] args) { 
+        byte myByte = 100; 
+        short myShort = 5000; 
+        int myInt = 100000; 
+        long myLong = 15000000000L;  
+        float myFloat = 3.14f;  
+        double myDouble = 19.99d;  
+        char myChar = 'A'; 
+        boolean myBoolean = true; 
+
+        System.out.println("byte: " + myByte); 
+        System.out.println("short: " + myShort); 
+        System.out.println("int: " + myInt); 
+        System.out.println("long: " + myLong); 
+        System.out.println("float: " + myFloat); 
+        System.out.println("double: " + myDouble); 
+        System.out.println("char: " + myChar); 
+        System.out.println("boolean: " + myBoolean); 
+    }
+} 
